ce6cc2240474f6d24e7cc77fe49c232464db10db
itsmeAyush <ayushksagar14@gmail.com>
2025-03-16 23:30:14 +0530

    frontend URL

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','https://qrcodeattendancee.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
